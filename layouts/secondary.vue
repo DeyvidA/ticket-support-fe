156cea92c4d49a35c7e8d6bbe868df385ade6e8a
DeyvidA <darauz@nicarao.agency>
2024-11-24 17:42:57 -0600

    Setup Shadcn

---
+++ layouts/secondary.vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Secondary Layout</h1>
+
+  <slot />
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,12 @@
+<template>
+  <section>
+    <p>This page will be displayed at the /about route.</p>
+    <Button @click="alert('yo coso')">Login</Button>
+  </section>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "secondary",
+});
+</script>
